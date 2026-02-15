cbf53ba0f08c1d4509401be54ac4f6176fe32153
Constantinos Giachalis <cgiachalis@outlook.com>
2026-02-15 17:31:06 +0200

    No need for ConfigError definition here, as it is now in-lined

--- tests/testthat/test-auto.R
+++ tests/testthat/test-auto.R
@@ -1,19 +1,9 @@
 test_that("Test 'driver_tiledb()' (auto via storr's test_driver)", {
 
-  ConfigError <- function(name, prev, requested) {
-    msg <- sprintf("Incompatible value for %s (existing: %s, requested: %s)",
-                   name, prev, requested)
-    structure(list(name = name,
-                   prev = prev,
-                   requested = requested,
-                   message = msg,
-                   call = NULL),
-              class = c("ConfigError", "error", "condition"))
-  }
 
   oldsize <- tiledb::get_allocation_size_preference()
   tiledb::set_allocation_size_preference(0.5 * 1024 * 1024)
-  on.exit(  tiledb::set_allocation_size_preference(oldsize))
+  on.exit(tiledb::set_allocation_size_preference(oldsize))
 
 
   uri <- file.path(withr::local_tempdir(), "test-driver")
