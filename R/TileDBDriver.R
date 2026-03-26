40ea004d294f94e331ae683582897df32c6258e2
Constantinos Giachalis <cgiachalis@outlook.com>
2026-03-26 15:35:05 +0200

    Docs: Methods overview

--- R/TileDBDriver.R
+++ R/TileDBDriver.R
@@ -7,7 +7,37 @@
 #' @title Generate a `TileDBDriver` Object
 #'
 #' @description An R6 class that represents a content addressable storage
-#' with 'storr' interface.
+#' with 'storr' interface using TileDB.
+#'
+#' This class is a [storr] compliant driver and its key  operations are briefly
+#' described below. For detail definitions, see `Methods` section.
+#'
+#' **Hash Management**
+#' - `get_hash` and `mget_hash`: Retrieve hash values for a given key and namespace.
+#' - `set_hash` and `mset_hash`: Set hash values with metadata like expiry date-times and notes.
+#' - `exists_hash`: Verify the existence of specific keys, namespaces
+#' - `del_hash`: Remove key-namespace pairs.
+#' - `delete_namespaces`: Clear namespaces or delete specified ones.
+#' - `list_namespaces`, `list_keys`: Retrieve all namespaces or keys for a given namespace.
+#'
+#' **Object Management**
+#' - `get_object` and `mget_object`: Fetch serialized R objects using hash values.
+#' - `set_object` and `mset_object`: Store serialized R objects.
+#' - `exists_object`: Verify the existence of specific objects.
+#' - `del_object`: Remove serialized objects.
+#' - `delete_unused_hashes`: Remove hashes that are not in active use.
+#' - `list_hashes`: Retrieve all hashes
+#' - `list_unused_hashes`: Identify unused hashes.
+#'
+#' **Key-Namespace Metadata**
+#'  - `get_keymeta`, `set_keymeta`, and `mset_keymeta`: Manage metadata such as
+#'   expiry times and notes for key-namespace pairs.
+#'
+#' **Expiration Management**
+#' - `expired_keys`: Retrieve the expired key namespace pairs.
+#' - `delete_expired_keys`: Delete all expired keys or for specific namespaces.
+#' - `num_expired_keys`: Get the number of expired keys or for specific namespaces.
+#' - `has_expired_keys`: Verify the existence of expired keys or for specific namespaces.
 #'
 #' @returns A `TileDBDriver`, `R6` object.
 #'
